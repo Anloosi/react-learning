983cec7c732287339424e6e5fcd0d8bedab517eb
prince kweku <kwekuasiedu2020@gmail.com>
2023-05-30 04:20:38 +0000

    second commit

--- my-app-wk/src/App.js
+++ my-app-wk/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import News from './components/News';
+import OpenField from './components/OpenField';
 
 function App() {
+  let Shownews = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      {Shownews?  <News/> : <OpenField/>}
+      <Footer/>
+
     </div>
   );
 }
---
+++ my-app-wk/src/components/OpenField.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function OpenField() {
+  return (
+    <div>OpenField</div>
+  )
+}
+
+export default OpenField
